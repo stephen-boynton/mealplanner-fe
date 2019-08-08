25ff878c25940d24979068ad5dc088090bff1222
Stephen Boynton <boyntonsc@gmail.com>
2019-08-08 06:37:02 -0400

    goin to work

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -4,22 +4,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const MealCardContainer = styled.div`
-display: flex;
-flex-direction: column;
-flex-basis: 48%;
-box-shadow: 1px 1px 1px lightgray;
-margin-bottom: 20px;
-background: white;
+  display: flex;
+  flex-direction: column;
+  flex-basis: 48%;
+  box-shadow: 1px 1px 1px lightgray;
+  margin-bottom: 20px;
+  background: white;
 `
 
 const MealName = styled.h2`
-font-size: 18px;
-margin: 0px;
-flex-basis: 100%;
-background: #00b894;
-color: white;
-padding: 20px;
-text-align: center;
+  font-size: 18px;
+  margin: 0px;
+  flex-basis: 100%;
+  background: #00b894;
+  color: white;
+  padding: 20px;
+  text-align: center;
 `
 
 const Description = styled.p`
@@ -34,31 +34,31 @@ const LastUsed = styled.p`
 `
 
 const ActionButtonContainer = styled.div`
-display: flex;
-justify-content: space-evenly;
-margin: 20px 0px;
+  display: flex;
+  justify-content: space-evenly;
+  margin: 20px 0px;
 `
 
 const FavIcon = styled(FontAwesomeIcon)`
-color: ${props => props.isFav ? "#d63031" : "#636e72"};
-border-radius: 50% 50%;
-padding: 10px;
+  color: ${props => props.isFav ? "#d63031" : "#636e72"};
+  border-radius: 50% 50%;
+  padding: 10px;
 
-:hover {
-  background: #b2bec3;
-  cursor: pointer;
-}
+  :hover {
+    background: #b2bec3;
+    cursor: pointer;
+  }
 `
 
 const AddIcon = styled(FontAwesomeIcon)`
-color: #fdcb6e;
-border-radius: 50% 50%;
-padding: 10px;
+  color: #fdcb6e;
+  border-radius: 50% 50%;
+  padding: 10px;
 
-:hover {
-  background: #b2bec3;
-  cursor: pointer;
-}
+  :hover {
+    background: #b2bec3;
+    cursor: pointer;
+  }
 `
 
 export default function MealCard(props) {
--- src/components/NewMealModal.js
+++ src/components/NewMealModal.js
@@ -1,42 +1,139 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
 const NewMealModalContainer = styled.div`
     display: flex;
     flex-direction: column;
+    align-items: center;
     position: absolute;
-    height: 75vh;
-    width: 40vw;
+    height: 100vh;
+    width: 100vw;
     background: white;
+    padding: 20px;
+    box-shadow: 12px 12px 12px lightgray;
+    overflow: scroll;
 `
 
 const MealForm = styled.form`
     display: flex;
     flex-direction: column;
-    flex-basis: 100%;
+    width: 60%;
+`
+
+const FormLabels = styled.label`
+    display: flex;
+    flex-direction: column;
+    margin: 10px;
+`
+
+const IngredientContainer = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const Icons = styled(FontAwesomeIcon)`
+    margin: 10px;
+`
+
+const Submit = styled.input`
+    width: 100px;
+    padding: 10px;
+    align-self: center;
+    border-radius: 20px;
+    background: #00b894;
+    color: white;
+    font-weight: bold;
+    font-size: 1rem;
+    border: none;
+
+    :hover {
+        box-shadow: 3px 3px 3px #b2bec3;
+    }
+
+    :active {
+        background: #55efc4;
+    }
+`
+
+const CancelButton = styled.button`
+padding: 10px;
+align-self: center;
+border-radius: 20px;
+background: white;
+/* background: #00b894; */
+/* color: white; */
+color: #636e72;
+border: 1px solid #636e72;
+font-weight: bold;
+font-size: 1rem;
+/* border: none; */
+
+:hover {
+  box-shadow: 3px 3px 3px #b2bec3;
+}
+
+:active {
+  background: #55efc4;
+}
+`
+
+const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: space-evenly;
 `
 
 export default function NewMealModal(props) {
-    const [ingredients, setIngredients] = useState(['']);
+    const [ingredients, setIngredients] = useState(['', '', '']);
 
-    const updateSelectedIngredient = (e) {
-        
+    const updateSelectedIngredient = i => e => {
+        const tempIngredients = [...ingredients];
+        tempIngredients[i] = e.target.value;
+        setIngredients(tempIngredients);
+    }
+
+    const deleteCurrentIngredient = i => e => {
+        const tempIngredients = [...ingredients];
+        setIngredients(tempIngredients.filter((x, i2) => i2 === i ? false : true));
+    }
+
+    const addIngredientAfter = i => () => {
+        const tempIngredients = [...ingredients];
+        tempIngredients.splice(i + 1, 0, '')
+        setIngredients(tempIngredients);
     }
 
     return (
         <NewMealModalContainer>
             <MealForm>
                 <h2>Add New Meal</h2>
-                <label>
+                <FormLabels>
                     Meal Name
                     <input />
-                </label>
-                <label>
+                </FormLabels>
+                <FormLabels>
+                    Description
+                    <input />
+                </FormLabels>
+                <FormLabels>
                     Ingredients
-                    {ingredients.map((ing, ind) => <input key={ind} onChange={updateSelectedIngredient} type="text" val={ing} />)}
-                </label>
+                    {ingredients.map((ing, ind) =>
+                        <IngredientContainer key={ind}>
+                            <input onChange={updateSelectedIngredient(ind)} type="text" value={ing} />
+                            <Icons onClick={deleteCurrentIngredient(ind)} icon={faMinus} />
+                            <Icons onClick={addIngredientAfter(ind)} icon={faPlus} />
+                        </IngredientContainer>
+                    )}
+                </FormLabels>
+                <FormLabels>
+                    Directions
+                    <textarea />
+                </FormLabels>
+                <ButtonContainer>
+                    <Submit type="submit" value="Save" />
+                    <CancelButton>Cancel</CancelButton>
+                </ButtonContainer>
             </MealForm>
         </NewMealModalContainer>
     )
